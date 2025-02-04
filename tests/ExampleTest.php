a08b30137817dc6649dd99579ee7bf3dd98445aa
DeoxysTheGod <quentinb2004@gmail.com>
2025-02-04 16:02:35 +0100

    test init

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+  public function testAddition()
+  {
+    $this->assertEquals(2, 1 + 1);
+  }
+}
